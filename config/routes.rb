a4c53ba6fd425e2ade050a2e12d47901346dbe27
Savannah C <sgcorrell@gmail.com>
2019-08-22 17:38:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/welcome' => 'users#welcome'
 
 end
