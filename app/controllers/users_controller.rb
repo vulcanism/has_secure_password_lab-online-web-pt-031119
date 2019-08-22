613b57f01537a473d0aea87c356b521b659eef0a
Savannah C <sgcorrell@gmail.com>
2019-08-22 17:39:11 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,13 @@ class UsersController < ApplicationController
   end
   
   def create
-    
+    user = User.new(user_params)
+      if user.save
+        session[:user_id] = user.id
+        redirect_to '/home'
+      else
+        redirect_to '/signup'
+      end
   end
   
   def welcome
