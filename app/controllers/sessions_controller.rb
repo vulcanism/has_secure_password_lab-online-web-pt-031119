992192bd3cb865ad3511b5cb28d5a1317e93c05c
Savannah C <sgcorrell@gmail.com>
2019-08-22 17:32:31 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
   
+  def create
+      @user = User.find_by(name: params[:name])
+      if @user
+        return head(:forbidden) unless @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to '/home'
+      else
+        redirect_to '/login'
+      end
+    end
+  
 end
